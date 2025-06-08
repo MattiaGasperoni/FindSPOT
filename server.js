d18a08481ad82efdec86da54c921efd7dc52f1fa
Mattia Gasperoni <mattiagasperoni02@gmail.com>
2025-06-08 21:23:40 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/api/parcheggi', (req, res) => {
+  res.sendFile(__dirname + '/data/parcheggi.geojson');
+});
+
+app.listen(port, () => {
+  console.log(`Server avviato su http://localhost:${port}`);
+});
