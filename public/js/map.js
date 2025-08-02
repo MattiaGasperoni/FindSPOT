73fa867c414df0757e9b45d3f864d1197ab523b7
Mattia Gasperoni <mattiagasperoni02@gmail.com>
2025-08-02 18:55:32 +0200

    Modificato il design del popup per scegliere il parcheggio da modificare

--- public/js/map.js
+++ public/js/map.js
@@ -327,7 +327,7 @@ class ParkingMap
           layer.bindPopup(`
             <div class="edit-popup-container">
               <strong class="edit-popup-title">${feature.properties.name || ""}</strong>
-              <button onclick="editParking('${feature.properties['@id']}')" class="edit-popup-button">Modify</button>
+              <button onclick="editParking('${feature.properties['@id']}')" class="edit-popup-button">Edit</button>
             </div>
           `, { closeButton: false });
 
